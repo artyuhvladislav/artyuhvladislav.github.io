2e7373ecaa398c94e6aaae278d98f34dbe5bf7dd
artyuhvladislav <workto22@gmail.com>
2019-08-14 20:12:12 +0300

    chd1
    q

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-let mobileMenu=document.getElementById("mobile").onclick = function() {open()};
-let langChange=document.getElementById("lang").onclick = function() {lang()};
-function open(){
-  document.getElementById("menu").classList.toggle("open");
-  document.getElementById("burger-line").classList.toggle("burger-line-active");
-};
+
 let hiddenElement = document.getElementById("links");
 let btn = document.querySelector('.button-contact');
 
