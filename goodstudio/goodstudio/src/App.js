00abb1aca9b3f4d373d96673b82205401d823f3e
artyuhvladislav <workto22@gmail.com>
2020-07-30 12:01:10 +0300

    add: create Calendar component

--- goodstudio/goodstudio/src/App.js
+++ goodstudio/goodstudio/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import './App.css';
+import Calendar from './Component/Calendar/Calendar';
 
 function App() {
   return (
     <div className="App">
-      
+        <Calendar />
     </div>
   );
 }
---
+++ goodstudio/goodstudio/src/Component/Calendar/Calendar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+const Calendar = (props) => {
+    return <div>
+        <input type="date" />
+    </div>
+}
+export default Calendar
