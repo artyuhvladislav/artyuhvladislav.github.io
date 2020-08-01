53ce7983b17473e8919a656c469b575b1704fab4
artyuhvladislav <workto22@gmail.com>
2020-08-01 18:24:47 +0300

    deploy app

---
+++ goodstudio/goodstudio/build/precache-manifest.aeb9992c8b1677d1b68b9e256501f283.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43c3b00a3505deaee228aa238330d52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ea817e9870982194a84",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b1dc04e718147201b457",
+    "url": "/static/js/2.b5a23d36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b5a23d36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ea817e9870982194a84",
+    "url": "/static/js/main.ca876fac.chunk.js"
+  },
+  {
+    "revision": "5d6746718abff3c5e50f",
+    "url": "/static/js/runtime-main.b85c045e.js"
+  }
+]);
